d0320151ea82c9d53370cf4511457dbe806a3be9
Nick Woodhams <nicholas.woodhams@gmail.com>
2013-08-13 08:56:34 -0700

    Changed to postgres db

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 CSRF_ENABLED = True
 SECRET_KEY = 'balancedmarketplacelove'
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'postgresql://marketplace:coolkicksman@127.0.0.1/marketplace'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 DEBUG = True
